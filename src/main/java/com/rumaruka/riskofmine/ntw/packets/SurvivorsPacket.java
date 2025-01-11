8b2a5d41107b11d310f72e996869784da5fbb6ea
Rumaruka <rumarukakunnya@gmail.com>
2025-01-11 20:29:27 +0500

    -Add SurvivorsPacket

--- src/main/java/com/rumaruka/riskofmine/ntw/packets/SurvivorsPacket.java
+++ src/main/java/com/rumaruka/riskofmine/ntw/packets/SurvivorsPacket.java
@@ -1,16 +1,55 @@
 package com.rumaruka.riskofmine.ntw.packets;
 
+import com.rumaruka.riskofmine.api.Survivors;
+import com.rumaruka.riskofmine.common.cap.Lunar;
+import com.rumaruka.riskofmine.common.entity.player.IPlayerSurvivorsBridge;
+import com.rumaruka.riskofmine.utils.ROMUtils;
+import io.netty.buffer.Unpooled;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.neoforge.network.handling.IPayloadContext;
 
 import static com.rumaruka.riskofmine.RiskOfMine.rl;
 
-public class SurvivorsPacket implements CustomPacketPayload {
+public record SurvivorsPacket(int entityID) implements CustomPacketPayload {
     public static final Type<SurvivorsPacket> TYPE = new Type<>(rl("survivors"));
 
-    public static final StreamCodec<FriendlyByteBuf, SurvivorsPacket> CODEC = StreamCodec.unit(new SurvivorsPacket());
+    public static final StreamCodec<FriendlyByteBuf, SurvivorsPacket> CODEC = StreamCodec.composite(
+            ByteBufCodecs.INT, SurvivorsPacket::entityID,
 
+
+            SurvivorsPacket::new);
+    public void handle(IPayloadContext ctx) {
+        ctx.enqueueWork(() -> {
+
+
+            Entity entity = ROMUtils.getLvL().getEntity(entityID());
+            if (entity != null) {
+                if (entity instanceof Player player) {
+                    if (player instanceof IPlayerSurvivorsBridge survivorsBridge){
+                        Survivors survivors = survivorsBridge.riskofmine$getSurvivor().survivors();
+                        FriendlyByteBuf byteBuf = new FriendlyByteBuf(Unpooled.buffer());
+                        byteBuf.writeUtf(entity.getStringUUID());
+                        byteBuf.writeInt(entityID());
+                        byteBuf.writeFloat(survivors.getHealth());
+                        byteBuf.writeFloat(survivors.getSpeed());
+                        byteBuf.writeFloat(survivors.getHealth_regen());
+                        byteBuf.writeFloat(survivors.getDamage());
+                        byteBuf.writeFloat(survivors.getArmor());
+                    }
+                }
+
+
+
+            }
+
+
+        });
+    }
     @Override
     public Type<? extends CustomPacketPayload> type() {
         return TYPE;
